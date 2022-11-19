d8cc3ee6ab2fdb7d6475dff52d4d7f7253205717
koheitakahashi <426koohei@gmail.com>
2022-11-19 14:56:32 +0900

    現在のブラウザの URL とタイトルからノートを作成するための文字列を生成できるようにした

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,50 @@
 import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { useState, useEffect } from "react";
+import { runAppleScript } from "run-applescript";
+
+type Link = {
+  title: string;
+  url: string;
+}
 
 type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
+  noteTitle: string;
+  noteBody: string;
 };
 
 export default function Command() {
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
+  const frontMostBrowserLink = async (): Promise<Link> => {
+    const titleAndUrl = await runAppleScript(`
+      tell application "System Events" to set frontApp to name of first process whose frontmost is true
+      if frontApp = "Google Chrome"
+        using terms from application "Google Chrome"
+          tell application frontApp to set currentTabTitle to title of active tab of front window
+          tell application frontApp to set currentTabUrl to URL of active tab of front window
+        end using terms from
+        return currentTabTitle & "\n" & currentTabUrl
+      end if
+    `);
+
+    const [originalTitle, originalUrl] = titleAndUrl.split("\n");
+
+    return { title: originalTitle, url: originalUrl }
+  };
+
+  useEffect(() => {
+    const initializeStore = async () => {
+      const link = await frontMostBrowserLink();
+      setTitle(link.title);
+
+      const body = `---\ntags: learned\n---\n\n created_at:${new Date().toISOString()}\n\n[${link.title}](${link.url})`;
+      setBody(body);
+    };
+
+    initializeStore();
+  }, []);
+
   function handleSubmit(values: Values) {
     console.log(values);
     showToast({ title: "Submitted form", message: "See logs for submitted values" });
@@ -23,18 +58,8 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
-      </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
+      <Form.TextField id="noteTitle" title="Note title" value={title} onChange={setTitle} />
+      <Form.TextArea id="noteBody" title="Note body" value={body} onChange={setBody} />
     </Form>
   );
 }
